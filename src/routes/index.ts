bd264a6b05e92ae529b4289f6e3672b222ddc056
Bruno <brunoferrazsabino@gmail.com>
2024-09-19 18:03:07 -0300

    update 19082024

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,20 @@
 import express from 'express';
+import z from 'zod';
 
 const router = express.Router()
 
 
 router.get('/', (req, res)=> {
-    res.json({"text": 'Ola mundo 2'});
+    res.json({"text": 'Ola mundo 23'});
 });
+
+router.get('/api/linhas', (req, res) => {
+    const linesBusSchema = z.string();
+
+    const result = linesBusSchema.safeParse(req.params);
+    if(!result.success) {
+        return res.json({ erros: "Dados invalidos"});
+    }
+})
 
 export default router;
